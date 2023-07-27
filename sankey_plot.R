e30207a0082544f77dd01d318d73e080f23bc675
chenzg5 <104574824+chenzg5@users.noreply.github.com>
2023-07-27 09:40:05 +0800

    Add files via upload

---
+++ sankey_plot.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(ggalluvial)
+library(readxl)
+library(dplyr)
+df <- read_excel("prognostic_sankey.xlsx")
+df <- group_by(df,Cluster,PINK.score,IPI.score) %>% summarise(., count = n())
+
+
+ggplot(as.data.frame(df),
+       aes(axis1 = Cluster, axis2 = PINK.score, axis3 = IPI.score,
+           y= count)) +
+  geom_flow(width = 0.2,
+            curve_type = "sine",
+            alpha = 0.5,
+            color = 'white',
+            size = 0.1)+
+  scale_x_discrete(limits = c("Cluster", "PINK.score", "IPI.score"), expand = c(.1, .05)) +
+  geom_alluvium(aes(fill = Cluster)) + 
+  geom_stratum(width = 0.20)+
+  geom_text(stat = "stratum", aes(label = after_stat(stratum)))+ 
+  #theme_minimal() 
+  scale_fill_manual(values = c("#bcbcbc","#803c96","#00a1d1","#f09121","#dd292a"))+#自定义颜色 
+  theme_void()
+  #ggtitle("Sankey-diagram of the clinical characteristics and molecular subtypes")
+
+
